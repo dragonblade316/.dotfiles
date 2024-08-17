4c363fd06755bf11b87e8889c64ee167319f1626
dragonblade316 <dragonblade316@yahoo.com>
2024-08-17 09:55:27 -0600

    leaving

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 if status --is-interactive
 	#thing that makes terminal look good
 	#set -Ux STARSHIP_DISTRO "  "
-	#starship init fish | source
+	starship init fish | source
 
 	#multiplexer
 	if set -q ZELLIJ
