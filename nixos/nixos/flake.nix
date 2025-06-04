81c3888fa427b48f434e8df692594df27c746ce0
dragonblade316 <dragonblade316@gmail.com>
2025-06-04 10:49:01 -0600

    added nixos config

---
+++ nixos/nixos/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.dragonblade316 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+      ];
+    };
+  };
+}
