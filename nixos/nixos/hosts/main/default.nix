a2db0c20670f1fc29422efd00996f607e63b4433
Teddy <dragonblade316@gmail.com>
2026-05-17 00:26:17 -0600

    177 (current) 2026-05-17 00:26:06  26.05.20260505.549bd84 6.18.26 49.7 GB

--- nixos/nixos/hosts/main/default.nix
+++ nixos/nixos/hosts/main/default.nix
@@ -115,12 +115,12 @@
   virtualisation.virtualbox.host.enable = true;
   virtualisation.vmware.host.enable = true;
   #
-  services.ollama = {
-    enable = true;
-    # Optional: preload models, see https://ollama.com/library
-    loadModels = ["qwen2.5-coder:1.5b"];
-    package = pkgs.ollama-rocm;
-  };
+  # services.ollama = {
+  #   enable = true;
+  #   # Optional: preload models, see https://ollama.com/library
+  #   loadModels = ["qwen2.5-coder:1.5b"];
+  #   package = pkgs.ollama-rocm;
+  # };
 
   services.llama-cpp = {
     enable = true;
