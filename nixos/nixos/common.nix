459f1dbd5a9511ebd4446f4b7c782e7365d426ee
Teddy <dragonblade316@gmail.com>
2026-08-16 12:16:18 -0600

    188 (current) 2026-08-15 00:51:59  26.11.20260629.b5aa0fb 6.18.37 46.0 GB

--- nixos/nixos/common.nix
+++ nixos/nixos/common.nix
@@ -496,8 +496,10 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+  };
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
